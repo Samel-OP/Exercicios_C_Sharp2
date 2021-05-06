7f282866f60e5a54ac624d53d217f49a011b0e17
Samel <samueldeoliveirapereira1@gmail.com>
2021-05-06 00:39:42 -0300

    Desafio

---
+++ ConsoleFibonacci/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleFibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fibonacci = 0;
+            int fibonacciTwo = 1;
+            int total = 0;
+
+            for (total = 0; total <= 610; total = fibonacci + fibonacciTwo)
+            {
+                fibonacci = fibonacciTwo;
+
+                fibonacciTwo = total;
+
+                Console.WriteLine("Contador Fibonacci: " + total);
+            }
+        }
+    }
+}
