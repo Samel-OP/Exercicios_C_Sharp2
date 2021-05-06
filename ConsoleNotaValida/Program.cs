b614231c701ea08305cf6593c58077ef92fe46ed
Samel <samueldeoliveirapereira1@gmail.com>
2021-05-06 00:37:07 -0300

    Exercício_1

---
+++ ConsoleNotaValida/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleNotaValida
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool respostaCorreta = false;
+
+            while (respostaCorreta == false)
+            {
+                Console.WriteLine("Escreva uma nota (entre 0 a 10): ");
+                int resposta = int.Parse(Console.ReadLine());
+
+                if (resposta >= 0 && resposta <= 10)
+                {
+                    Console.WriteLine("Nota enviada com sucesso!");
+                    respostaCorreta = true;
+                }
+                else
+                {
+                    Console.WriteLine("Nota inválida!");
+                    respostaCorreta = false;
+                }
+            }
+        }
+    }
+}
